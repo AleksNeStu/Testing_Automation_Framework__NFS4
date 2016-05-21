1adac6763bfccc99269d28c7d3ff8d92db3ea802
AleksNeStu <a.v.nesterovich@gmail.com>
2016-05-22 02:01:24 +0300

    -create new clear view -fixed -tested -eh!!!!

--- payload/test1.py
+++ payload/test1.py
@@ -2,63 +2,83 @@
 # -*- coding: utf-8 -*-
 """
 
-#######################################################################
-# Test #1: NFSv4 test case [Limits the length of the ACLs attributes] #
-# Developed by AleksNeStu                                             #
-#######################################################################
+########################################################################################################
+# Test #1: [NFSv4 - Test the maximum number of ACEs (Access Control Entries) supported by file system] #
+# Developed by AleksNeStu                                                                              #
+########################################################################################################
 
 """
-from pinger import *        #real-time ping module with 5 counts
-from hosts_info import *    #client and server info print
-import time                     #pauses
-#import platform            #in order to get uname info#from socket import *
-
-print("""
-=========================================================================
-\nTest #1: NFSv4 test case [Limits the length of the ACLs attributes]
-   Limits the length of the ACLs attributes:
-   - Up to 35 ACL entries per ACL page
-   - Up to 6 character in attribute "user"
-   - Up to 10 character in attribute "domain"\n
-=========================================================================
-""")
-################GET INFO###########################################
-print("In order to run test case fill the required details:")
-#Get from the local client and the remote server info (hostname, IP, uname) and print
-server = str(raw_input("    1) Hostname of the remote NFSv4 server: "))
-print "   ",server
-#Get the length of the ACLs attributes for testing
-while True:
-    try:
-        acl_max = int(raw_input("    2) Length of the ACLs attributes (30..35): ")) #30 <= acl_max <= 35:
-        if acl_max not in range(30, 36):
-            raise ValueError("Error! Enter correct information, a value in the range: 30..35")
-        break
-    except ValueError as acl_err:
-        print acl_err
-print "   ",acl_max
-#Print intro info
-time.sleep(1)
-print("""
-=========================================================================
-
-Great! The input data had been received! After 5 seconds the test will be started...
-""")
-################GET INFO###########################################
-time.sleep(5)
-################RUN TEST CASE###########################################
-#Get client and server info, display it
-get_hosts_info(server)
-time.sleep(1)
-#Ping via pinger of NFSv4 server
-pinger(str(server))
+from pinger import *         #real-time ping with 5 counts (display)
+from hosts_info import *     #input, get client and server info (display)
+from fstype_check import *   #get file system type
+from nfsexp_check import *   #inpunt, check exported dir (path) on server (display)
+from hostname_check import * #input, check hostname resolved (display)
+import time                  #for pauses
 
 print """
+=========================================================================
+
+Test #1: NFSv4 - Test the maximum number of ACEs (Access Control Entries)
+         supported by file system
+
+    Limits for the max number of the ACEs per individual file/directory:
+    - EXT2, EXT3, EXT4: 32
+    - XFS: 25
+    - GFS2: 25
+
+=========================================================================
+"""
+################GET INFO###########################################
+
+#i1 Set, check server hostname
+print "In order to run the test enter the required data: \n"
+print "    1) Hostname of the NFSv4 server: "
+server = hostnamecheck()			#input, check resolved hostname of the server (display)
+print "    NFSv4 server: ",server   #server = NFS server's hostname
+print
+
+#i2 Set, check (from client to server) path of exportfs on server
+print "    2) Path to the exported directory on NFSv4 server: "
+nfsexp = nfsexpcheck(server)
+print "    NFSv4 exported dir: ",nfsexp #view /nfs, /dirnfs, ...
+
+#Print test intro info
+time.sleep(3)
+print """
+Great! The input data have been received!
+After 7 seconds the test will be started...
+
+=========================================================================
+"""
+time.sleep(7)
+
+
+################RUN TEST###########################################
+
+#t1 Get client and server info
+gethostinfo(server)
+time.sleep(3)
+print
+
+#t2 Ping from client to server with 5 counts
+pinger(str(server))
+time.sleep(3)
+print
+
+#####################LOG AND PRINT RESULTS################################
+#print "Test #1 [PASSED]"
+print """
 ##########################################################################
+
 Test #1 [PASSED]
-In order to get more information:
-./logs/log_run.log" - execution log (detailed information)
-./logs/log_result.log - log with the results (PASSED, FAILED)
+
+    In order to get more information:
+    ./logs/log_run.log" - execution log (detailed information)
+    ./logs/log_result.log - log with the results (PASSED, FAILED)
+
 ##########################################################################
 """
+
 #print "Test #1 [FAILED]"
+
+#log test results
