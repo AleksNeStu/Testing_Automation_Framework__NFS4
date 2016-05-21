0522ee9e3627ae4ba1ed2d833b2431f7a9e3ee9e
AleksNeStu <a.v.nesterovich@gmail.com>
2016-05-22 02:00:36 +0300

    -fixed -release

--- payload/hosts_info.py
+++ payload/hosts_info.py
@@ -1,56 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-r"""Get info about local host and remote host (manual input)
+r"""Get info from client and server
 
 
-This module provides the geting information (Uname, Hostname, IP) from local host and remote host with display it.
-local host - auto
-remote host - after input hostname
-In order to use it need call:
-get_hosts_info(rmhost)
-rmhost - hostname of the remote hosst
+info: uname, hostname, IP
+client info - auto
+server info - after receive server hostname
 @Developed by AleksNeStu
 
 """
-import socket           #in order get IP, hostname (remote, local)
-import subprocess       #in order run External Command And Get Output
-#import platform        #uname + other
+import socket           #IP, hostname (remote, local)
+import subprocess       #parsing and display external commands
 
-def get_hosts_info(rmhost):
-    if not rmhost:
+def gethostinfo(host):
+    if not host:
         return
-#Get local server hostname, IP, uname
+#Get client info (hostname, IP, uname)
     client = socket.gethostname()
     client_ip = socket.gethostbyname(client)
-    client_uname_t = subprocess.Popen(["uname", "-a"], stdout=subprocess.PIPE)  #uname local >>> client_uname
+    client_uname_t = subprocess.Popen(["uname", "-srp"], stdout=subprocess.PIPE)  #uname local >>> client_uname
     client_uname = client_uname_t.communicate()                                 #uname local >>> client_uname
     #client_uname = platform.system(), platform.release(), platform.version(), platform.machine()
 
-#Get remote server hostname, IP
-    server_uname_t = subprocess.Popen(["rsh","-n", rmhost, "uname -a"], stdout=subprocess.PIPE)  #uname remote >>> sever_uname
+#Get server info (hostname, IP, uname)
+    server_uname_t = subprocess.Popen(["rsh","-n", host, "uname -srp"], stdout=subprocess.PIPE)  #uname remote >>> sever_uname
     server_uname = server_uname_t.communicate()
+    server_ip = socket.gethostbyname(host)
 
-    while True:
-        try:
-#           server = str(raw_input("Please enter the hostname of the remote host: "))  #rmserver
-            server_ip = socket.gethostbyname(rmhost)
-            break
-        except socket.gaierror:
-            print "Error! Enter correct information, for example: servername, smain, ..."
-            break
-#Print the received info
-    print("""
-=========================================================================
-    """)
-    print "[Client]"
-    print "Uname    : ",client_uname
-    print "Hostname : ",client
-    print "IP       : ",client_ip
+#Print the received from client and server info
+    print "    Client   : ",client_uname
+    print "    Hostname : ",client
+    print "    IP       : ",client_ip
     print
-    print "[Server]"
-    print "Uname    : ",server_uname
-    print "Hostname : ",rmhost
-    print "IP       : ",server_ip
-    print("""
-=========================================================================
-    """)
+    print "    Server   : ",server_uname
+    print "    Hostname : ",host
+    print "    IP       : ",server_ip
