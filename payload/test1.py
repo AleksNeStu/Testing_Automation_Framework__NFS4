68f28c5800e767cc5144a05c2f75ff0bbb0269f5
AleksNeStu <a.v.nesterovich@gmail.com>
2016-05-23 02:47:56 +0300

    -fixed of interactive menu for information input -worked appearance -tested

--- payload/test1.py
+++ payload/test1.py
@@ -2,24 +2,25 @@
 # -*- coding: utf-8 -*-
 """
 
-########################################################################################################
-# Test #1: [NFSv4 - Test the maximum number of ACEs (Access Control Entries) supported by file system] #
-# Developed by AleksNeStu                                                                              #
-########################################################################################################
+#########################################################################
+# NFSv4 - Test the maximum number of ACEs (Access Control Entries)		#
+#         supported by file system [Extended ACLs for UNIX]				#
+# @Developed by AleksNeStu												#
+#########################################################################
 
 """
 from pinger import *         #real-time ping with 5 counts (display)
 from hosts_info import *     #input, get client and server info (display)
-from fstype_check import *   #get file system type
 from nfsexp_check import *   #inpunt, check exported dir (path) on server (display)
 from hostname_check import * #input, check hostname resolved (display)
 import time                  #for pauses
+import subprocess
 
 print """
 =========================================================================
 
 Test #1: NFSv4 - Test the maximum number of ACEs (Access Control Entries)
-         supported by file system
+         supported by file system [Extended ACLs for UNIX]
 
     Limits for the max number of the ACEs per individual file/directory:
     - EXT2, EXT3, EXT4: 32
@@ -34,13 +35,34 @@ Test #1: NFSv4 - Test the maximum number of ACEs (Access Control Entries)
 print "In order to run the test enter the required data: \n"
 print "    1) Hostname of the NFSv4 server: "
 server = hostnamecheck()			#input, check resolved hostname of the server (display)
-print "    NFSv4 server: ",server   #server = NFS server's hostname
+print "    NFSv4 server: ",server   #server - NFS server's hostname
 print
 
-#i2 Set, check (from client to server) path of exportfs on server
+#i2 Set, check (from client to server via "showmount -e server") path of exportfs on server
 print "    2) Path to the exported directory on NFSv4 server: "
-nfsexp = nfsexpcheck(server)
-print "    NFSv4 exported dir: ",nfsexp #view /nfs, /dirnfs, ...
+nfs_exp = nfsexpcheck(server)		#nfs_exp - dir for export (will be mounted on client side)
+print "    NFSv4 exported dir: ",nfs_exp #view /nfs, /dirnfs, ...
+print
+
+#i3 Get, check type of the file system on server and set ACEs limits in accordance with it
+print "    3) NFSv4 server exported dir filesystem type & ACEs limits: "
+#Get fs type on "server" export dir "nfs_exp"
+p1 = subprocess.Popen(['cat', './fstype_check_l.py', ], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['rsh', server, 'python', '-', '-p', nfs_exp], stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+server_fs = p2.communicate()[0]			#server_fs - fs type for export dir "nfs_exp"
+#Set limits according fs type
+if server_fs == str('xfs'):
+	aces_max = 25
+else:
+	aces_max = 32
+print "    NFSv4 server fs type for exp dir ",nfs_exp," : ",server_fs
+print "    ACEs max count (UNIX extended ACLs): ", aces_max
+print
+
+#i4 Set the number of users and groups to be created on the NFSv4 server
+print "    4) The number of users and groups to be created on the NFSv4 server: "
+
 
 #Print test intro info
 time.sleep(3)
