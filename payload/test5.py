2c339d4dcb4731457704eeb1077952ae3a01b32f
AleksNeStu <a.v.nesterovich@gmail.com>
2016-05-31 11:29:47 +0300

    add header of test5 for future

--- payload/test5.py
+++ payload/test5.py
@@ -1,2 +1,27 @@
-print("Test #5")
-print('')
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+
+#####################################################################################################
+# NFSv4 - Stress test for a large number of random operations setting ACEs [NFSv4 ACLs] 	        #
+# @Developed by AleksNeStu												                            #
+#####################################################################################################
+
+"""
+
+print """
+=========================================================================
+
+Test #5: Stress test for a large number of random operations setting
+         ACEs (Access Control Entries) [NFSv4 ACLs]
+
+    The testing of correct operation of the services of NFSv4 server
+    with a large number of random operations setting ACEs [NFSv4 ACLs]
+
+    *** nfs4_setfacl, nfs4_editfacl
+
+=========================================================================
+
+COMING SOON!!!
+
+"""
